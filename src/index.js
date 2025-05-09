74a765fca308091d6614adc8a1c00bc297a6d7cb
juankdavila <juan.davila@tecnologicoliceocristiano.edu.ec>
2025-05-08 21:28:11 -0500

    Merge branch 'main' of https://github.com/juankdavila/todo.add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import routerTodos from "./routes/todos.routes.js";
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
